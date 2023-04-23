cdc6c080a9201d9f02c18d7771b4323500570f35
will-mccormack <will.88.mack@gmail.com>
2023-04-22 19:54:16 -0700

    WDM 4/22/2023 --> UCSDap -> creates UCSD_article_pages.json
    UCSDas -> Scrapes .json and populates
    subdirectory
    
    UCSD6 -> Deleted because outdated
    
    ** No graph just yet **

--- UCSD_pdf_dates.py
+++ UCSD_pdf_dates.py
@@ -1,45 +1,60 @@
 import requests
 from bs4 import BeautifulSoup
+import json
 from datetime import datetime
 
-def get_pdf_links(pdf_full_url):
-    """
-    Retrieves all PDF links from a given URL
+# Base URL for the UCSD newspaper archives
+base_url = "https://library.ucsd.edu/dc/"
 
-    Args:
-        url (str): The URL to retrieve PDF links from
+# Load the subpage links from the subpage_links.json file
+with open("subpage_links.json", "r") as f:
+    subpage_links = json.load(f)
 
-    Returns:
-        A list of PDF links (str)
-    """
-    # Retrieve HTML content from URL
-    response = requests.get(url)
+# List to store all the PDF links
+pdf_links = []
+
+# Loop through all the subpages and scrape the PDF links
+for subpage_link in subpage_links:
+    # Make a GET request to the subpage URL
+    response = requests.get(subpage_link)
+
+    # Create a BeautifulSoup object from the response HTML
     soup = BeautifulSoup(response.content, 'html.parser')
 
-    # Find all links in the HTML content
-    links = soup.find_all('a')
+    # Find all the PDF links on the page
+    pdf_links_on_page = soup.find_all('a', class_='dams-search-thumbnail-link') #pdf_links_on_page = soup.find_all('li', class_='dams-sr') 
 
-    # Retrieve all links that contain ".pdf"
-    pdf_links = [link['href'] for link in links if '.pdf' in link.get('href', '')]
+    # Loop through all the PDF links and extract the PDF URL
+    for pdf_link_on_page in pdf_links_on_page:
+        try:
+            # Extract the relative URL for the PDF
+            pdf_url = pdf_link_on_page['href']
 
-    # Return the PDF links
-    return pdf_links
+            pdf_url = pdf_url.replace("/dc/", "")
 
-def find_pdf_date(pdf_links):
-    pdf_dates = {}
-    for pdf_link in pdf_links:
-        pdf_url = pdf_link.get('href')
-        if pdf_url.endswith('/_1.pdf'):
-            date_url = pdf_url[:-7] # Remove the "/_1.pdf" suffix to get the date URL
-            date_request = requests.get(date_url)
-            date_soup = BeautifulSoup(date_request.text, 'html.parser')
-            try:
-                date_str = date_soup.find('div', class_='vc_tta-panel-body').find('strong').text.strip()
-                date_obj = datetime.strptime(date_str, '%B %d, %Y')
-                date_formatted = date_obj.strftime('%Y_%m_%d')
-                pdf_dates[pdf_url] = date_formatted
-            except ValueError:
-                print(f"Date format for {pdf_url} is unexpected, skipping...")
-    return pdf_dates
+            # Construct the absolute URL for the PDF file and add the "_1.pdf" suffix
+            date_url = f"{base_url}{pdf_url.split('?')[0]}"
+            print(date_url)
+            date_response = requests.get(date_url)
+            date_soup = BeautifulSoup(date_response.content, 'html.parser')
 
+            # Find the date element
+            date_elem = date_soup.find('dt', string='Creation Date').find_next_sibling('dd').find('ul', class_='unstyled').find('li').get_text(strip=True)
+            if date_elem:
+                date = date_elem
+            else:
+                date = None
 
+            # Check if the date element exists and formats the dates (YYYY_mm_dd)
+            if date_elem is not None:
+                date_obj = datetime.strptime(date_elem, '%B %d, %Y')
+                formatted_date = date_obj.strftime('%Y_%m_%d')
+                print(formatted_date)
+            else:
+                raise ValueError("Could not find date element")
+
+        except (KeyError, ValueError, AttributeError) as e:
+            # Handle any exceptions that occur
+            print(f"Error: {e}")
+            continue
+    
